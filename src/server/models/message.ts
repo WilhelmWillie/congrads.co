a6ef89c2729b7c3fd30539daab823ab8d1db6c89
Wilhelm Willie <wilhelmwillie@gmail.com>
2020-05-05 17:44:08 -0700

    fix typo in models

--- src/server/models/message.ts
+++ src/server/models/message.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
   senderName: String,
-  senderEmmail: {
+  senderEmail: {
     type: String,
     required: true,
   },
