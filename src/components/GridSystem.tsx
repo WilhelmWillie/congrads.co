db962467d242fcd906c6827b3788dcd53a43ee21
Wilhelm Willie <wilhelmwillie@gmail.com>
2020-05-10 14:13:44 -0700

    init work to setup design system

---
+++ src/components/GridSystem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  max-width: 1024px;
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export {
+  Container
+}
