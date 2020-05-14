5af889d6bdfafc200454666f43392788a2cb6658
Wilhelm Willie <wilhelmwillie@gmail.com>
2020-05-13 23:49:18 -0700

    fleshed out form UI

--- src/pages/msg/new.tsx
+++ src/pages/msg/new.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { Head, NavBar, Container, Footer, Form, FormSection, Label, TextInput, Button } from "../../components";
 
@@ -7,34 +8,59 @@ const New = () => {
     <>
       <Head title="Congrads - Create a message for a graduate" />
       <NavBar />
-      <Container>
-        <h1>Create a message</h1>
-        <p>Create a personalized message to your favorite graduate! Include photos, memories, and more in your message to add a personal touch.</p>
+      <StyledContainer>
+        <NewMessageDescription>
+          <h1>Create a message</h1>
+          <p>Create a personalized message to your favorite graduate! Include photos, memories, and more in your message to add a personal touch.</p>
+        </NewMessageDescription>
 
         <Form>
           <FormSection>
             <Label>Graduate's Name</Label>
-            <TextInput />
+            <TextInput placeholder="Jane Doe" />
           </FormSection>
 
           <FormSection>
             <Label>Graduate's School</Label>
-            <TextInput />
+            <TextInput placeholder="University of Southern California" />
+          </FormSection>
+
+          <FormSection>
+            <Label>Message Tagline</Label>
+            <TextInput placeholder="An awesome graduate who deserves the world" />
+          </FormSection>
+
+          <FormSection>
+            <Label>Your Name</Label>
+            <TextInput placeholder="John Doe" />
           </FormSection>
 
           <FormSection>
             <Label>Your Message</Label>
-            <TextInput as="textarea" rows={4}/>
+            <TextInput as="textarea" rows={4} placeholder="Congrats on your graduation..." />
           </FormSection>
 
           <FormSection>
-            <Button>Submit</Button>
+            <Button primary big>Submit</Button>
           </FormSection>
         </Form>
-      </Container>
+      </StyledContainer>
       <Footer />
     </>
   )
 };
+
+const StyledContainer = styled(Container)`
+  padding-bottom: 128px;
+`;
+
+const NewMessageDescription = styled.div`
+  padding: 48px 0 64px;
+
+  p {
+    font-size: 24px;
+    line-height: 36px;
+  }
+`;
 
 export default New;
